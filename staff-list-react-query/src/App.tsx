4b5cbbcef098b922b208071381f4c8f6a7afe0ea
AyamotoKohei <koh14taka2@gmail.com>
2021-11-25 23:48:28 +0900

    add: Appとindexの実装

--- staff-list-react-query/src/App.tsx
+++ staff-list-react-query/src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import { VFC } from 'react';
+import { Navigate, Route, Routes } from 'react-router';
+
+import Home from 'containers/pages/Home';
+import Members from 'containers/pages/Members';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: VFC = () => (
+  <>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/:orgCode/members" element={<Members />} />
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  </>
+);
 
 export default App;
--- staff-list-react-query/src/index.tsx
+++ staff-list-react-query/src/index.tsx
@@ -1,17 +1,36 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import 'semantic-ui-css/semantic.min.css';
+import './index.css';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 0,
+      suspense: true,
+    },
+    mutations: {
+      retry: 0,
+    },
+  },
+});
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <App />
+      {process.env.NODE_ENV === 'development' && (
+        <ReactQueryDevtools initialIsOpen={false} />
+      )}
+    </QueryClientProvider>
+  </BrowserRouter>,
+  document.getElementById('root') as HTMLElement,
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
